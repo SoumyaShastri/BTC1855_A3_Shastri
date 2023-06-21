0f28aea849f468247a87a247597b8c854dcafddc
Soumya Shastri <soumyashastri11@gmail.com>
2023-06-21 15:53:54 -0400

    added function to display user progress

--- A3_Shastri.R
+++ A3_Shastri.R
@@ -8,8 +8,24 @@ file_path <- "word_inventory.txt"
 #* colClasses = "character" argument ensures the content is read as character strings
 wordList <- read.table(file_path, header=FALSE, sep = "\n", colClasses = "character")
 
+
 # One word will be sampled from the list as the secret word 
 secretWord <- sample(wordList, size = 1)
+
+# Create function to give user a visual clue of their progress
+visualClue <- function(word, guesses) {
+  masked_word <- "" #Initialize empty string
+  for (letter in strsplit(word, "")[[1]]) { #takes each letter in the 'word' and splits into individual characters
+    if (tolower(letter) %in% guesses) { #tolower function allows user to enter and the program to accept both upper and lower case letters
+      masked_word <- paste(masked_word, letter, sep = " ") #if letter found in 'guesses' then letter is revealed
+    } else {
+      masked_word <- paste(masked_word, "_", sep = " ") #else letter remains hidden with "_"
+    }
+  }
+  cat(masked_word, "\n") #this prints the final word representation
+}
+
+
 #Max number of incorrect guesses allowed
 max_incorrect_guesses <- 10
 #Tracking correct and incorrect guesses 
@@ -30,5 +46,4 @@ cat("Good luck!")
 ##TO DO: 
 #* inform of remaining tries
 #* ensure character entered is letter 
-#* Both capital and lowercase letters are accepted 
 #* inform user of incorrect or correct tries - graphical representation?? 
